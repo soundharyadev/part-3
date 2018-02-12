ab565d32c6e86b5bac610e386c50416afcf946b0
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2018-02-12 16:56:42 +0530

    Create fibo.c

---
+++ fibo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int n,a=0,b=1,temp,i;
+	printf("enter the number");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		printf("%d\n",t1);
+		temp=a+b;
+		a=b;
+		b=temp;
+	}
+return 0;
+}
