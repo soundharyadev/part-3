b2eef9c36dd983e0a9ffca58e8d38caf3b9cc4f8
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2018-02-26 16:52:54 +0530

    Create multiple of 13.c

---
+++ multiple of 13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+  printf("Enter the number:");
+  scanf("%d",&a);
+  if(a%13==0)
+  {
+  printf("It is multiple of 13");
+  }
+  else
+  {
+  printf("It is not multiple of 13");
+  }
+  }
