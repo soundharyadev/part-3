ff6c2b43febcef9316ba1b9c5c3245e6b18bb210
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2018-02-26 16:02:15 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c[200];
+int i;
+printf("Enter the binary string:");
+scanf("%s",&c);
+for(i=0;s[i]='\0';i++)
+{
+if(s[i]>='0'&&s[i]<='1')
+{
+printf("yes");
+}
+else
+{
+printf("No");
+}
+}
+getch();
+}
