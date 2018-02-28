eb10e03b9e8d6b005e3706c5cd2537bfb944da41
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2018-02-28 15:41:34 +0530

    Create factors.c

---
+++ factors.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,i;
+clrscr();
+printf("Enter the number");
+scanf("%d",&a);
+for(i=0;i<=n;i++)
+{
+if(a%i==0)
+{
+printf("%d",&i);
+}
+}
+getch();
+}
+
