2c765005e9e413b45d9be2256b3a56f468693863
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2018-03-02 15:56:25 +0530

    Update vowels.c

--- vowels.c
+++ vowels.c
@@ -2,14 +2,17 @@
 #include<conio.h>
 void main()
 {
-char a[200];
+char a[100];
 int i=0,count=0;
 clrscr();
 scanf("%s",&a);
 while(a[i]!='\0')
 {
-a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u';
-count++;
+if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+{
+count=1;
+}
+i++;
 }
 if(count==1)
 {
@@ -21,4 +24,5 @@ printf("No");
 }
 getch();
 }
+
 
