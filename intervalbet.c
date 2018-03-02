e8e37894954ab998c287e8007e0c3207e52b93fc
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2018-03-02 16:21:15 +0530

    Create intervalbet.c

---
+++ intervalbet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n,l,r;
+printf("Enter the number n:");
+scanf("%d",&n);
+printf("enter the number l:");
+scanf("%d",&l);
+printf("Enter the number r:");
+scanf("%d",&r);
+if(l<n&&n<r)
+{
+printf("Yes");
+}
+else
+{
+printf("No");
+}
+}
