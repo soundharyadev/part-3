f593f1fe6cb2a73ba9fde8b5348d3ff442022dd2
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2018-03-12 15:43:01 +0530

    Create farm.c

---
+++ farm.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+float l,b,a;
+printf("enter the length of the farm:");
+scanf("%f",&l);
+printf("enter the breadth of the farm:");
+scanf("%f",&b);
+a=l*b;
+printf("%f",a);
+
+}
