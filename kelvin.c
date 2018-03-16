ab8b4fb5efb0ec8f766962f5d2695fa7f8a2206c
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2018-03-16 15:45:40 +0530

    Create kelvin.c

---
+++ kelvin.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int celsius, kelvin=273;
+clrscr();
+printf("Enter the celsius temperature:");
+scanf("%d",&celsius);
+temp=celsius+kelvin;
+printf("The temperature in the kelvin is %d",temp);
+getch();
+}
